b6fdccd9e1a5af49483bcd777e8f8fcdfdc10ac2
Siva R <166374356+Siva1309@users.noreply.github.com>
2024-04-29 14:06:02 +0530

    Update Exp4_Dflipflop.v

--- Exp4_Dflipflop.v
+++ Exp4_Dflipflop.v
@@ -1,8 +1,9 @@
-6(iii): D FLIP FLOP 
-module dff( clk,d,q); 
-input clk,d; 
-output reg q; 
-
-
-
+module D_Ff(q,d,clk,rest);
+output q;
+input d, clk, reset;
+reg q;
+always @(posedge reset or negedge if (reset)
+q = 1'b0;
+else
+q = d;
 endmodule
